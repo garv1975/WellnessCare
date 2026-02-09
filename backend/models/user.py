163add129702d35827641a4bf4c37e1ecba4805f
Garv <ggarv44@gmail.com>
2026-02-09 21:35:42 +0530

    updated cors

--- backend/models/user.py
+++ backend/models/user.py
@@ -10,7 +10,7 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     
     def __init__(self, email):
         self.email = email
