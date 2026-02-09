8119e684d3bd1c106be207f3b3792eff7da6adae
Garv <ggarv44@gmail.com>
2026-02-09 12:54:05 +0530

    first commit

--- backend/models/appointment.py
+++ backend/models/appointment.py
@@ -7,7 +7,7 @@ class Appointment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'), nullable=False)
-    time = db.Column(db.String(100), nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
     status = db.Column(db.String(50), default='Scheduled')
     reason = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
